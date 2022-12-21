bbf9b662d52ee1849b01959a1b535cf39b7d61af
Arvis Ceirulis <103632556+ArvisCe@users.noreply.github.com>
2022-12-21 19:14:12 +0200

    Add files via upload

---
+++ windows-forms-apps/Hangman/Karatavas/Karatavas/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: skolotajs
+ * Date: 23.11.2022
+ * Time: 17:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Karatavas
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Guess = new System.Windows.Forms.TextBox();
+			this.Wrongs = new System.Windows.Forms.Label();
+			this.Vword = new System.Windows.Forms.Label();
+			this.livesT = new System.Windows.Forms.Label();
+			this.status = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// Guess
+			// 
+			this.Guess.Font = new System.Drawing.Font("Microsoft Sans Serif", 80F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Guess.Location = new System.Drawing.Point(134, 341);
+			this.Guess.MaxLength = 1;
+			this.Guess.Name = "Guess";
+			this.Guess.Size = new System.Drawing.Size(146, 128);
+			this.Guess.TabIndex = 0;
+			this.Guess.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			this.Guess.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// Wrongs
+			// 
+			this.Wrongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Wrongs.ForeColor = System.Drawing.Color.Red;
+			this.Wrongs.Location = new System.Drawing.Point(12, 260);
+			this.Wrongs.Name = "Wrongs";
+			this.Wrongs.Size = new System.Drawing.Size(401, 78);
+			this.Wrongs.TabIndex = 2;
+			this.Wrongs.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// Vword
+			// 
+			this.Vword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Vword.Location = new System.Drawing.Point(12, 152);
+			this.Vword.Name = "Vword";
+			this.Vword.Size = new System.Drawing.Size(401, 37);
+			this.Vword.TabIndex = 4;
+			this.Vword.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			// 
+			// livesT
+			// 
+			this.livesT.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.livesT.Location = new System.Drawing.Point(12, 15);
+			this.livesT.Name = "livesT";
+			this.livesT.Size = new System.Drawing.Size(401, 48);
+			this.livesT.TabIndex = 5;
+			this.livesT.Text = "♥ ♥ ♥";
+			this.livesT.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// status
+			// 
+			this.status.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.status.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.status.Location = new System.Drawing.Point(12, 63);
+			this.status.Name = "status";
+			this.status.Size = new System.Drawing.Size(401, 89);
+			this.status.TabIndex = 6;
+			this.status.Text = "label2";
+			this.status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(368, 406);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(45, 54);
+			this.button1.TabIndex = 7;
+			this.button1.Text = "⟲ ";
+			this.button1.UseCompatibleTextRendering = true;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(425, 472);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.status);
+			this.Controls.Add(this.livesT);
+			this.Controls.Add(this.Vword);
+			this.Controls.Add(this.Wrongs);
+			this.Controls.Add(this.Guess);
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.Name = "MainForm";
+			this.Text = "Karatavas";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label status;
+		private System.Windows.Forms.Label livesT;
+		private System.Windows.Forms.Label Vword;
+		private System.Windows.Forms.Label Wrongs;
+		private System.Windows.Forms.TextBox Guess;
+	}
+}
---
+++ windows-forms-apps/Hangman/Karatavas/Karatavas/MainForm.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: skolotajs
+ * Date: 23.11.2022
+ * Time: 17:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Karatavas
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+			Guess.Text = Guess.Text.ToLower();
+			if(Guess.Text != "")
+			{
+				string guess = Guess.Text;
+				Guess.Text = ""; 
+				if(word.Contains(guess))
+				{
+					string placeHolder = "";
+					for(int i = 0; i < word.Length; i++)
+					{
+						if(word[i] == guess[0])
+						{
+							placeHolder += guess[0];
+						}
+						else
+						{
+							placeHolder += Vword.Text[i];
+						}
+					}
+					Vword.Text = placeHolder;
+				}
+				else
+				{
+					if(!Wrongs.Text.Contains(guess))
+					{
+						Wrongs.Text += " "+guess[0];
+						lives -= 1;
+						livesT.Text = "";
+						for(int i = 0; i < lives; i++)
+						{
+							livesT.Text += "♥";
+						}
+					}
+				}
+			}			
+			if(!Vword.Text.Contains("︵"))
+			{
+				Win();
+			}
+			if(lives == 0)
+			{
+				Lose();
+			}
+
+		}
+		
+		void Label1Click(object sender, EventArgs e)
+		{
+			
+		}
+		
+		void SubmitClick(object sender, EventArgs e)
+		{
+			if(Guess.Text != "")
+			{
+				string guess = Guess.Text;
+				Guess.Text = ""; 
+			}
+		}
+		public string word;
+		public string[] words = System.IO.File.ReadAllLines(@"C:\Users\skolotajs\Documents\SharpDevelop Projects\Karatavas\Karatavas\WordList.txt");
+		public int lives = 5;
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			status.Text = "";
+			status.ForeColor = Color.Black;
+			livesT.Text = "♥ ♥ ♥ ♥ ♥";
+			Guess.Enabled = true;
+			Random rnd = new Random();
+			word = words[rnd.Next(0, words.Length)];
+			Vword.Text = "";	
+			foreach (char letter in word) 
+			{
+				Vword.Text += "︵";	
+			}
+		}
+		public void Lose()
+		{
+			Guess.Enabled = false;
+			status.Text = "You lose";
+			status.ForeColor = Color.Red;
+			Vword.Text = "Correct word: "+word;
+		}
+		public void Win()
+		{
+			Guess.Enabled = false;
+			status.Text = "You win";
+			status.ForeColor = Color.Green;
+			Vword.Text = "You guess the word: "+word;
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			status.Text = "";
+			status.ForeColor = Color.Black;
+			livesT.Text = "♥ ♥ ♥ ♥ ♥";
+			Guess.Enabled = true;
+			Random rnd = new Random();
+			word = words[rnd.Next(0, words.Length)];
+			Vword.Text = "";	
+			lives = 5;
+			Wrongs.Text = "";
+			foreach (char letter in word) 
+			{
+				Vword.Text += "︵";	
+			}
+		}
+	}
+}
